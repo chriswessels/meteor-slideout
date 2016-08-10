64c8b455f3b8bf9431d12c8e116edf999d6f6e17
Chris Wessels <undefined.za@gmail.com>
2016-08-10 11:47:14 +0100

    Squashed 'lib/' changes from 8ae55b2..8179c1e
    8179c1e Add new events:  and .
    96b335e Remove alert.
    d0fea20 git status
    0339591 Build dist v0.1.12
    06a7922 Update docs.
    d0fae6c Updated tests.
    fbb35b1 Use translateX+will-change instead of translate3d.
    de02546 Added tolerance option docs. #110
    781fd92 Added fixed header docs. #111
    5ce87b7 Added slideout only on mobile devices docs #130.
    8c628f9 Added :heart: emoji.
    5302d57 Merge branch 'master' of github.com:Mango/slideout
    dc6a761 Updated uglify-js dependency. #128
    6965ea6 Added coverage badge.
    b70fec1 Added code coverage. Updated uglify-js dependency. #128
    e0fb1d7 Updated shields.
    ca4d9e6 Builded v0.1.11
    ce0c72e Fixed: Scrolling not working on mobile devices. #132
    eacc970 Updated cdnjs link.
    a294324 Updated component version.
    39df0d7 Builded v0.1.10.
    b20658a Merge pull request #114 from Mango/feature-destroy-instance
    08b0ba8 Updated decouple dependency.
    b84b4e7 Added destroy method. closes #55
    
    git-subtree-dir: lib
    git-subtree-split: 8179c1ec40e5a40a0ef9f600ad9ba1e70718af8f

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Slideout.js [![NPM version][npm-image]][npm-link] [![Build status][travis-image]][travis-link] [![devDependency status][devdeps-image]][devdeps-link]
+# Slideout.js
+
+[![NPM version][npm-image]][npm-link] [![License][lic-image]][npm-link] [![Build status][travis-image]][travis-link] [![Coverage Status][coverage-image]][coverage-link] [![Dependency status][deps-image]][deps-link] [![devDependency status][devdeps-image]][devdeps-link] [![downloads][dt-image]][npm-link]
 
 > A touch slideout navigation menu for your mobile web apps.
 
@@ -22,7 +24,7 @@
 Slideout is available on cdnjs
 
 ```html
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/0.1.9/slideout.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/0.1.11/slideout.min.js"></script>
 ```
 
 Also you can use one of many package managers
@@ -79,6 +81,7 @@ body {
 .slideout-panel {
   position:relative;
   z-index: 1;
+  will-change: transform;
 }
 
 .slideout-open,
@@ -141,6 +144,7 @@ Then you just include Slideout.js and create a new instance with some options:
       .slideout-panel {
         position:relative;
         z-index: 1;
+        will-change: transform;
       }
 
       .slideout-open,
@@ -205,7 +209,7 @@ Create a new instance of `Slideout`.
 - `[options.duration]` (Number) - The time (milliseconds) to open/close the slideout. Default: `300`.
 - `[options.fx]` (String) - The CSS effect to use when animating the opening and closing of the slideout. Default: `ease`.
 - `[options.padding]` (Number) - Default: `256`.
-- `[options.tolerance]` (Number) - Default: `70`.
+- `[options.tolerance]` (Number) - The number of `px` needed for the menu can be opened completely, otherwise it closes. Default: `70`.
 - `[options.touch]` (Boolean) - Set this option to false to disable Slideout touch events. Default: `true`.
 - `[options.side]` (String) - The side to open the slideout (`left` or `right`). Default: `left`.
 
@@ -244,6 +248,13 @@ Returns `true` if the slideout is currently open, and `false` if it is closed.
 
 ```js
 slideout.isOpen(); // true or false
+```
+
+### Slideout.destroy();
+Cleans up the instance so another slideout can be created on the same area.
+
+```js
+slideout.destroy();
 ```
 
 ### Slideout.enableTouch();
@@ -288,14 +299,28 @@ An instance of Slideout emits the following events:
 - `close`
 - `beforeopen`
 - `open`
+- `translatestart`
 - `translate`
+- `translateend`
 
-The slideout emits `translate` event only when it is opening/closing via touch events.
+The slideout emits `translatestart`, `translate` and `translateend` events only when it is opening/closing via touch events.
 
 ```js
-slideout.on('translate', function(translated) {
-  console.log(translated); // 120 in px
+slideout.on('translatestart', function() {
+  console.log('Start');
 });
+
+slideout.on('translate', function(translated) {
+  console.log('Translate: ' + translated); // 120 in px
+});
+
+slideout.on('translateend', function() {
+  console.log('End');
+});
+
+// 'Start'
+// 'Translate 120'
+// 'End'
 ```
 
 ## npm-scripts
@@ -349,7 +374,56 @@ var slideout = new Slideout({
 });
 ```
 
-## With ❤ by
+### How to enable slideout only on mobile devices.
+
+You should use `mediaqueries`:
+```css
+@media screen and (min-width: 780px) {
+  .slideout-panel {
+    margin-left: 256px;
+  }
+
+  .slideout-menu {
+    display: block;
+  }
+
+  .btn-hamburger {
+    display: none;
+  }
+}
+```
+Demo: http://codepen.io/anon/pen/xGEdvQ?editors=100
+
+### How to use slideout with a fixed header.
+
+You should define a two class names: `fixed` and `fixed-open`.
+```css
+.fixed {
+  backface-visibility: hidden;
+  position: fixed;
+  z-index:2;
+  transition: transform 300ms ease;
+}
+
+.fixed-open {
+  transform: translate3d(256px, 0px, 0px);
+}
+```
+
+Then, using slideout's events you should add / remove the `fixed-open` class name from the fixed element.
+```js
+slideout.on('beforeopen', function() {
+  document.querySelector('.fixed').classList.add('fixed-open');
+});
+
+slideout.on('beforeclose', function() {
+  document.querySelector('.fixed').classList.remove('fixed-open');
+});
+```
+
+Demo: http://codepen.io/anon/pen/NqJGBp
+
+## With :heart: by
 - Guille Paz (Front-end developer | Web standards lover)
 - E-mail: [guille87paz@gmail.com](mailto:guille87paz@gmail.com)
 - Twitter: [@pazguille](http://twitter.com/pazguille)
@@ -358,9 +432,15 @@ var slideout = new Slideout({
 ## License
 MIT license. Copyright © 2015 [Mango](http://getmango.com).
 
-[npm-image]: https://img.shields.io/npm/v/slideout.svg?style=flat
+[npm-image]: https://img.shields.io/npm/v/slideout.svg
+[lic-image]: https://img.shields.io/npm/l/slideout.svg
 [npm-link]: https://npmjs.org/package/slideout
-[travis-image]: https://img.shields.io/travis/Mango/slideout.svg?style=flat
+[travis-image]: https://img.shields.io/travis/Mango/slideout.svg
 [travis-link]: https://travis-ci.org/Mango/slideout
-[devdeps-image]: https://img.shields.io/david/dev/mango/slideout.svg?style=flat
-[devdeps-link]: https://david-dm.org/mango/slideout#info=peerDependencies
+[deps-image]: https://img.shields.io/david/mango/slideout.svg
+[deps-link]: https://david-dm.org/mango/slideout
+[devdeps-image]: https://img.shields.io/david/dev/mango/slideout.svg
+[devdeps-link]: https://david-dm.org/mango/slideout#info=devDependencies
+[dt-image]: https://img.shields.io/npm/dt/slideout.svg
+[coverage-image]: https://img.shields.io/coveralls/Mango/slideout.svg
+[coverage-link]: https://coveralls.io/github/Mango/slideout
--- index.js
+++ index.js
@@ -111,7 +111,9 @@ Slideout.prototype.open = function() {
  */
 Slideout.prototype.close = function() {
   var self = this;
-  if (!this.isOpen() && !this._opening) { return this; }
+  if (!this.isOpen() && !this._opening) {
+    return this;
+  }
   this.emit('beforeclose');
   this._setTransition();
   this._translateXTo(0);
@@ -143,7 +145,8 @@ Slideout.prototype.isOpen = function() {
  */
 Slideout.prototype._translateXTo = function(translateX) {
   this._currentOffsetX = translateX;
-  this.panel.style[prefix + 'transform'] = this.panel.style.transform = 'translate3d(' + translateX + 'px, 0, 0)';
+  this.panel.style[prefix + 'transform'] = this.panel.style.transform = 'translateX(' + translateX + 'px)';
+  return this;
 };
 
 /**
@@ -151,6 +154,7 @@ Slideout.prototype._translateXTo = function(translateX) {
  */
 Slideout.prototype._setTransition = function() {
   this.panel.style[prefix + 'transition'] = this.panel.style.transition = prefix + 'transform ' + this._duration + 'ms ' + this._fx;
+  return this;
 };
 
 /**
@@ -162,7 +166,7 @@ Slideout.prototype._initTouchEvents = function() {
   /**
    * Decouple scroll event
    */
-  decouple(doc, 'scroll', function() {
+  this._onScrollFn = decouple(doc, 'scroll', function() {
     if (!self._moved) {
       clearTimeout(scrollTimeout);
       scrolling = true;
@@ -175,60 +179,83 @@ Slideout.prototype._initTouchEvents = function() {
   /**
    * Prevents touchmove event if slideout is moving
    */
-  doc.addEventListener(touch.move, function(eve) {
+  this._preventMove = function(eve) {
     if (self._moved) {
       eve.preventDefault();
     }
-  });
+  };
+
+  doc.addEventListener(touch.move, this._preventMove);
 
   /**
    * Resets values on touchstart
    */
-  this.panel.addEventListener(touch.start, function(eve) {
-
-    if (typeof eve.touches === 'undefined') { return; }
+  this._resetTouchFn = function(eve) {
+    if (typeof eve.touches === 'undefined') {
+      return;
+    }
 
     self._moved = false;
     self._opening = false;
     self._startOffsetX = eve.touches[0].pageX;
     self._preventOpen = (!self._touch || (!self.isOpen() && self.menu.clientWidth !== 0));
-  });
+  };
+
+  this.panel.addEventListener(touch.start, this._resetTouchFn);
 
   /**
    * Resets values on touchcancel
    */
-  this.panel.addEventListener('touchcancel', function() {
+  this._onTouchCancelFn = function() {
     self._moved = false;
     self._opening = false;
-  });
+  };
+
+  this.panel.addEventListener('touchcancel', this._onTouchCancelFn);
 
   /**
    * Toggles slideout on touchend
    */
-  this.panel.addEventListener(touch.end, function() {
+  this._onTouchEndFn = function() {
     if (self._moved) {
+      self.emit('translateend');
       (self._opening && Math.abs(self._currentOffsetX) > self._tolerance) ? self.open() : self.close();
     }
     self._moved = false;
-  });
+  };
+
+  this.panel.addEventListener(touch.end, this._onTouchEndFn);
 
   /**
    * Translates panel on touchmove
    */
-  this.panel.addEventListener(touch.move, function(eve) {
+  this._onTouchMoveFn = function(eve) {
 
-    if (scrolling || self._preventOpen || typeof eve.touches === 'undefined') { return; }
+    if (scrolling || self._preventOpen || typeof eve.touches === 'undefined') {
+      return;
+    }
 
     var dif_x = eve.touches[0].clientX - self._startOffsetX;
     var translateX = self._currentOffsetX = dif_x;
 
-    if (Math.abs(translateX) > self._padding) { return; }
+    if (Math.abs(translateX) > self._padding) {
+      return;
+    }
 
     if (Math.abs(dif_x) > 20) {
+
       self._opening = true;
 
       var oriented_dif_x = dif_x * self._orientation;
-      if (self._opened && oriented_dif_x > 0 || !self._opened && oriented_dif_x < 0) { return; }
+
+      if (self._opened && oriented_dif_x > 0 || !self._opened && oriented_dif_x < 0) {
+        return;
+      }
+
+      if (!self._moved) {
+        self.emit('translatestart');
+      }
+
       if (oriented_dif_x <= 0) {
         translateX = dif_x + self._padding * self._orientation;
         self._opening = false;
@@ -238,22 +265,53 @@ Slideout.prototype._initTouchEvents = function() {
         html.className += ' slideout-open';
       }
 
-      self.panel.style[prefix + 'transform'] = self.panel.style.transform = 'translate3d(' + translateX + 'px, 0, 0)';
+      self.panel.style[prefix + 'transform'] = self.panel.style.transform = 'translateX(' + translateX + 'px)';
       self.emit('translate', translateX);
       self._moved = true;
     }
 
-  });
+  };
 
+  this.panel.addEventListener(touch.move, this._onTouchMoveFn);
+
+  return this;
 };
 
+/**
+ * Enable opening the slideout via touch events.
+ */
 Slideout.prototype.enableTouch = function() {
   this._touch = true;
   return this;
 };
 
+/**
+ * Disable opening the slideout via touch events.
+ */
 Slideout.prototype.disableTouch = function() {
   this._touch = false;
+  return this;
+};
+
+/**
+ * Destroy an instance of slideout.
+ */
+Slideout.prototype.destroy = function() {
+  // Close before clean
+  this.close();
+
+  // Remove event listeners
+  doc.removeEventListener(touch.move, this._preventMove);
+  this.panel.removeEventListener(touch.start, this._resetTouchFn);
+  this.panel.removeEventListener('touchcancel', this._onTouchCancelFn);
+  this.panel.removeEventListener(touch.end, this._onTouchEndFn);
+  this.panel.removeEventListener(touch.move, this._onTouchMoveFn);
+  doc.removeEventListener('scroll', this._onScrollFn);
+
+  // Remove methods
+  this.open = this.close = function() {};
+
+  // Return the instance so it can be easily dereferenced
   return this;
 };
 
